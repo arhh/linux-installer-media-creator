7be02af2013ef1105c3920e15f970307d64f5dc4
arhh <aulee7@icloud.com>
2020-06-13 18:15:24 +0100

    Update script to adhere to popular shell scripting conventions
    Variables are all upper-case. Check for argument count is simplified and moved down.

--- linux_installer_creator.sh
+++ linux_installer_creator.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
 set -e
 
-# Check if user supplied correct number of arguments for script to execute correctly:
-if [[ "$1" != "--help" ]] && [[ $# -ne 2 ]]; then
-    printf "Error: Incorrect number of arguments passed.\n"
-    printf "Syntax: bootable_linux_usb.sh 'iso-image' 'target-device'\n"
-    exit 1
-fi
-
-# # Check if user requested help and display help:
+# Check if user requested help and display help:
 if [[ "$1" = "--help" ]]; then
     printf "\nWrite Linux ISO images to removable drives using macOS.\n"
     printf "\nUSAGE:\n"
     printf "\tbootable_linux_usb.sh 'iso-image' 'target-drive'\n"
     printf "\nNOTES:\n"
-    printf "\tThe image passed to this script should be in ISO format, there is no need to convert the image to an IMG file as this script will do that automatically.\n"
+    printf "\tThe image passed to this script should be in ISO format.\n"
     exit 0
 fi
 
-# Get user-specified ISO image and target drive to write the image to:
-iso_file=$1
-target_drive=$2
+# Display an error if incorrect number of arguments passed:
+if [[ $# -ne 2 ]]; then
+    printf "Error: Incorrect number of arguments passed.\n"
+    printf "Syntax: bootable_linux_usb.sh 'iso-image' 'target-device'\n"
+    exit 1
+fi
 
-# Create raw path to target drive for faster write:
-raw_target_drive="${target_drive:0:5}r${target_drive:5}"
+# Get user-specified ISO image and target drive to write the image to:
+ISO_FILE=$1
+TARGET_DRIVE=$2
+
+# Create "raw" path to target drive for faster write:
+RAW_TARGET_DRIVE="${TARGET_DRIVE:0:5}r${TARGET_DRIVE:5}"
 
 # Convert the ISO image to an IMG for writing to target drive:
-hdiutil convert -format UDRW -o output.img $iso_file
+hdiutil convert -format UDRW -o output.img $ISO_FILE
 
 # Unmount the target drive:
-diskutil unmountDisk $target_drive
+diskutil unmountDisk $TARGET_DRIVE
 
 # Write the image to the unmounted drive:
-sudo dd if=output.img.dmg of=$raw_target_drive bs=1m
+sudo dd if=output.img.dmg of=$RAW_TARGET_DRIVE bs=1m
 
 # Clean up by removing the IMG file:
 rm output.img.dmg
