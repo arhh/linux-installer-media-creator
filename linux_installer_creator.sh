dc42780c9d25b33d3543bfdd27b52402667a901a
arhh <aulee7@icloud.com>
2020-06-13 20:12:24 +0100

    Update help message, error message, commands
    The help message now mentions that raw unbuffered writes are used in dd command. The error message usage statement dynamically updates based on the name of the command, this was required to prevent confusion if the error was not updated following a change in the script's name. hdiutil now has the ISO file immediately following the convert verb, as suggested by hdiutil man page.dd command has block size of 4m instead of 1m and is followed by sync command if it succeeds.

--- linux_installer_creator.sh
+++ linux_installer_creator.sh
@@ -5,16 +5,18 @@ set -e
 if [[ "$1" = "--help" ]]; then
     printf "\nWrite Linux ISO images to removable drives using macOS.\n"
     printf "\nUSAGE:\n"
-    printf "\tbootable_linux_usb.sh 'iso-image' 'target-drive'\n"
+    printf "\t%s ISO_IMAGE TARGET_DRIVE\n" $0
     printf "\nNOTES:\n"
     printf "\tThe image passed to this script should be in ISO format.\n"
+    printf "\tThis script uses the unbuffered 'raw' disk write (i.e. rdiskN).\n"
+    printf "\tRoot privileges required.\n"
     exit 0
 fi
 
 # Display an error if incorrect number of arguments passed:
 if [[ $# -ne 2 ]]; then
-    printf "Error: Incorrect number of arguments passed.\n"
-    printf "Syntax: bootable_linux_usb.sh 'iso-image' 'target-device'\n"
+    printf "%s: Incorrect number of arguments passed.\n" $0
+    printf "usage: %s ISO_IMAGE TARGET_DRIVE\n" $0
     exit 1
 fi
 
@@ -25,14 +27,14 @@ TARGET_DRIVE=$2
 # Create "raw" path to target drive for faster write:
 RAW_TARGET_DRIVE="${TARGET_DRIVE:0:5}r${TARGET_DRIVE:5}"
 
-# Convert the ISO image to an IMG for writing to target drive:
-hdiutil convert -format UDRW -o output.img $ISO_FILE
+# Convert the ISO image to a "UDIF R/W image" for writing to target drive:
+hdiutil convert $ISO_FILE -format UDRW -o output.img
 
 # Unmount the target drive:
 diskutil unmountDisk $TARGET_DRIVE
 
 # Write the image to the unmounted drive:
-sudo dd if=output.img.dmg of=$RAW_TARGET_DRIVE bs=1m
+sudo dd if=output.img.dmg of=$RAW_TARGET_DRIVE bs=4m && sync
 
 # Clean up by removing the IMG file:
 rm output.img.dmg
