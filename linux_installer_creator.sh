c03ffa980ace72eb12b433d17906b1d0c65506a1
arhh <arhed7@yahoo.com>
2021-07-04 17:42:52 +0100

    Add logic for Linux to unmount partitions before writing
    The help text has also been updated in this commit.

--- linux_installer_creator.sh
+++ linux_installer_creator.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
-set -e
 
-# Initialise constants
 TEMP_IMG="output.img"
 OS_TYPE=$(uname)
+IS_MACOS=0
 
-# Check if user requested help and display help
+if [[ "$OS_TYPE" = "Darwin" ]]; then
+    IS_MACOS=1
+fi
+
 if [[ "$1" = "--help" ]]; then
     printf "\\nWrite Linux ISO images to removable drives.\\n"
     printf "\\nUSAGE:\\n"
@@ -14,42 +16,39 @@ if [[ "$1" = "--help" ]]; then
     printf "\\tThe image passed to this script should be in \"ISO\" format.\\n"
     printf "\\tThis script uses the unbuffered \"raw\" disk write (i.e. rdiskN) when run in macOS.\\n"
     printf "\\tRoot privileges required.\\n"
+    printf "\\tA temporary file equal to the source iso will be created in the current directory called \"output.img.dmg\" in macOS.\\n"
     exit 0
 fi
 
-# Display an error if incorrect number of arguments passed
 if [[ $# -ne 2 ]]; then
     printf "%s: Incorrect number of arguments passed.\\n" "$0"
     printf "usage: %s ISO_IMAGE TARGET_PATH\\n" "$0"
     exit 1
 fi
 
-# Get user-specified ISO image and target drive to write the image to
-SOURCE_PATH=$1
+ISO_IMAGE=$1
 TARGET_PATH=$2
 
+SOURCE=$ISO_IMAGE
 TARGET_RAW=$TARGET_PATH
-if [[ "$OS_TYPE" = "Darwin" ]]; then
-    printf "converting..."
-    # If macOS then use "raw" path to target to improve write speed:
+
+if [[ "$IS_MACOS" ]]; then
     TARGET_RAW="${TARGET_PATH:0:5}r${TARGET_PATH:5}"
-    # Convert the ISO image to a "UDIF R/W image" for writing to target drive
-    hdiutil convert "$SOURCE_PATH" -format UDRW -o "$TEMP_IMG"
-    # Unmount the target drive
+    hdiutil convert "$ISO_IMAGE" -format UDRW -o "$TEMP_IMG"
     diskutil unmountDisk "$TARGET_PATH"
-fi
-
-if [[ "$OS_TYPE" = "Darwin" ]]; then
-    # Write the image to the unmounted drive
-    sudo dd if="${TEMP_IMG}.dmg" of="$TARGET_RAW" bs=4m && sync
-elif [[ "$OS_TYPE" = "Linux" ]]; then
-    sudo dd if="$SOURCE_PATH" of="$TARGET_RAW" bs=4M status=progress && sync
 else
-    printf "Error: Unrecognised operating system \"%s\"\\n" "$OSTYPE"
-    exit 1
+    umount "${TARGET_PATH}?"
 fi
 
-# Clean up by removing the IMG file
+if [[ "$IS_MACOS" ]]; then
+    DD_COMMAND="sudo dd if="${TEMP_IMG}.dmg" of="$TARGET_RAW" bs=4m && sync"
+else
+    DD_COMMAND="sudo dd if=\"$ISO_IMAGE\" of=\"$TARGET_RAW\" bs=4M status=progress conv=fsync"
+fi
+
+eval $DD_COMMAND
+
+# Remove temporary img file in macOS
 if [[ -f "${TEMP_IMG}.dmg" ]]; then
     rm "${TEMP_IMG}.dmg"
 fi
